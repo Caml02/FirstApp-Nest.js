c9ab133d1e216cd093015f5aba4dbc1ca42d8a5d
Cristian Morales <cristianmorales3006@gmail.com>
2023-08-12 14:17:17 -0700

    Finish Validations

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,23 @@
 import { TaskStatus } from "../task.entity"
+import { IsNotEmpty, IsString, MinLength, IsOptional, IsIn} from 'class-validator';
 
 export class createTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     tittle: string
     description: string
 }
 
 export class updateTaskDto {
+    @IsString()
+    @IsOptional()
     tittle?: string
+    @IsString()
+    @IsOptional()
     description?: string
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status?: TaskStatus
 }
